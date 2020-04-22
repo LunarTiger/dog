3f0adef433427300799c9b270798779ca6531c86
LunarTiger <lunartiger@live.com>
2020-04-22 08:53:22 -0400

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,10 +8,10 @@ var message = database.ref('dog/html');
 message.on('value', (function(snapshot) {
 	var messageVal = snapshot.val();
 	if(messageVal){
-		if(messageVal==2){
+		if(messageVal==1){
 			document.getElementById('more-child').innerHTML = "<p>You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.</p>";
 		}
-		if(messageVal==3){
+		else if(messageVal==2){
 			document.getElementById('more-child').innerHTML = "<p>My owner is away from the house. You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>. You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.</p>"
 		}
 		else{
