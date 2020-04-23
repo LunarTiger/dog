cc74814a8534a51cf5f328d72b5348930c9907dd
LunarTiger <lunartiger@live.com>
2020-04-23 14:03:32 -0400

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,9 @@
-var htmldefault = "You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
-var htmlhome = "My owner is home!";
-var htmlawayd = "My owner is away from the house. You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>.";
-var htmlawaym = "My owner is away from the house. You can try contacting Shelva at <a href='tel:+13048204338'>1-304-820-4338</a>.";
+var place = [
+	"You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.",
+	"My owner is home!",
+	"My owner is away from the house. You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>.",
+	"My owner is away from the house. You can try contacting Shelva at <a href='tel:+13048204338'>1-304-820-4338</a>."
+];
 try{
 	var config = {
 		databaseURL: "https://lunar-home.firebaseio.com/",
@@ -14,23 +16,23 @@ try{
 		var messageVal = snapshot.val();
 		if(messageVal){
 			if(messageVal=="home"){
-				document.getElementById('more-info').innerHTML = htmlhome+"&nbsp;"+htmldefault;
+				document.getElementById('more-info').innerHTML = place[1]+" "+place[0];
 			}
 			else if(messageVal=="awayd"){
-				document.getElementById('more-info').innerHTML = htmlawayd+"&nbsp;"+htmldefault;
+				document.getElementById('more-info').innerHTML = place[2]+" "+place[0];
 			}
 			else if(messageVal=="awaym"){
-				document.getElementById('more-info').innerHTML = htmlawaym+"&nbsp;"+htmldefault;
+				document.getElementById('more-info').innerHTML = place[3]+" "+place[0];
 			}
 			else{
-				document.getElementById('more-info').innerHTML = htmldefault;
+				document.getElementById('more-info').innerHTML = place[0];
 			}
 		}
 		if(!messageVal){
-			document.getElementById('more-info').innerHTML = htmldefault;
+			document.getElementById('more-info').innerHTML = place[0];
 		}
 	}));
 }
 catch{
-	document.getElementById('more-info').innerHTML = "Script failed. "+htmldefault;
+	document.getElementById('more-info').innerHTML = "Script failed. "+place[0];
 }
