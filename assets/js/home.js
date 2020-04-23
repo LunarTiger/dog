3517ba0b40c9f18e62dc915922976f6e56a6c251
LunarTiger <lunartiger@live.com>
2020-04-23 12:52:42 -0400

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,7 @@
+var htmldefault = "You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+var htmlhome = "My owner is home!";
+var htmlawayd = "My owner is away from the house. You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>.";
+var htmlawaym = "My owner is away from the house. You can try contacting Shelva at <a href='tel:+13048204338'>1-304-820-4338</a>.";
 try{
 	var config = {
 		databaseURL: "https://lunar-home.firebaseio.com/",
@@ -10,23 +14,23 @@ try{
 		var messageVal = snapshot.val();
 		if(messageVal){
 			if(messageVal=="home"){
-				document.getElementById('more-info').innerHTML = "My owner is home! You can contact him at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+				document.getElementById('more-info').innerHTML = htmlhome+"&nbsp;"+htmldefault;
 			}
 			else if(messageVal=="awayd"){
-				document.getElementById('more-info').innerHTML = "My owner is away from the house. You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>. You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+				document.getElementById('more-info').innerHTML = htmlawayd+"&nbsp;"+htmldefault;
 			}
 			else if(messageVal=="awaym"){
-				document.getElementById('more-info').innerHTML = "My owner is away from the house. You can try contacting Shelva at <a href='tel:+13048204338'>1-304-820-4338</a>. You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+				document.getElementById('more-info').innerHTML = htmlawaym+"&nbsp;"+htmldefault;
 			}
 			else{
-				document.getElementById('more-info').innerHTML = messageVal;
+				document.getElementById('more-info').innerHTML = htmldefault;
 			}
 		}
 		if(!messageVal){
-			document.getElementById('more-info').innerHTML = "You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+			document.getElementById('more-info').innerHTML = htmldefault;
 		}
 	}));
 }
 catch{
-	document.getElementById('more-info').innerHTML = "You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
+	document.getElementById('more-info').innerHTML = "Script failed. "+htmldefault;
 }
