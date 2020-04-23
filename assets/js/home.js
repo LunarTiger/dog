302141febc2faab8e8ead10726e7aa6c62be0e0f
LunarTiger <lunartiger@live.com>
2020-04-23 11:23:58 -0400

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,21 +5,21 @@ try{
 	firebase.initializeApp(config);
 	var database = firebase.database();
 	//Extra info
-	var message = database.ref('dog/html');
-	message.on('value', (function(snapshot) {
-		var messageVal = snapshot.val();
-		if(messageVal){
-			if(messageVal=="home"){
+	var status = database.ref('dog/status');
+	status.on('value', (function(snapshot) {
+		var statusVal = snapshot.val();
+		if(statusVal){
+			if(statusVal=="home"){
 				document.getElementById('more-info').innerHTML = "My owner is home! You can contact him at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
 			}
-			else if(messageVal=="away"){
+			else if(statusVal=="away"){
 				document.getElementById('more-info').innerHTML = "My owner is away from the house. You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>. You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
 			}
 			else{
-				document.getElementById('more-info').innerHTML = messageVal;
+				document.getElementById('more-info').innerHTML = statusVal;
 			}
 		}
-		if(!messageVal){
+		if(!statusVal){
 			document.getElementById('more-info').innerHTML = "You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.";
 		}
 	}));
