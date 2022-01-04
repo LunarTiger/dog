8d3717e2cd7ce2d40859357b8741932d13cd7cfe
Ryan Fowler-Hughes <lunartiger@live.com>
2022-01-04 18:01:44 -0500

    :tada:

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,5 @@
 var place = [
-	"You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through his <a href='/contact' id='contact' target='_blank'>contact page</a>.",
+	"You can contact my owner at <a href='tel:+15406926899' id='phone'>1-540-692-6899</a> or through its <a href='/contact' id='contact' target='_blank'>contact page</a>.",
 	"My owner is home!",
 	"My owner is away from the house.",
 	"You can try contacting Lanny at <a href='tel:+15403279023'>1-540-327-9023</a>.",
